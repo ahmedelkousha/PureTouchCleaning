185a1ff608695dbc4c238d88ff3d9978e99f9817
ahmedelkousha <elkoushaservice@gmail.com>
2026-02-09 05:31:26 +0200

    optimize imgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpeg: { quality: 70 },
+      png: { quality: 70 },
+      webp: { quality: 70 },
+      avif: { quality: 60 },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
